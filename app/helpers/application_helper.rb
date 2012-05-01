cdd6f9c2595b99636e2d9cb79fadffac99e0eb94
Debashis Giri <dgiri@crri.co.in>
2012-05-01 22:16:07 +0530

    Added home controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def select_tab_class(parm)
+		case parm
+			when "album"
+			 if params[:controller] == "albums"
+			 	"active"
+			 end
+			when "home"
+				if params[:controller] == "home"
+					"active"
+				end			
+		end
+	end
 end
