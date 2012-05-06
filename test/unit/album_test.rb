766302b074a343254fe96d0d49f4230d677ecceb
Debashis Giri <dgiri@crri.co.in>
2012-05-06 12:21:01 +0530

    update test

--- test/unit/album_test.rb
+++ test/unit/album_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-	#should validate_presence_of :name
-	#should validate_presence_of :description
 	should have_many(:photos)
+	should validate_presence_of :name
+	should validate_presence_of :description
 end
